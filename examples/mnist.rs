65c7e00f74f83787c223e6e84bcef1cadf73755f
Jack17432 <ChrisGraham908@gmail.com>
2023-02-04 00:20:12 +1300

    Updated mnist.rs to do the one_hot_encoding correctly using usize

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -12,9 +12,9 @@ fn main() {
 }
 
 fn create_mnist_dataset(trn_len: u32, tst_len: u32, val_len: u32) -> (
-    Array3<f64>, Array2<f64>,   // Training data, label
-    Array3<f64>, Array2<f64>,   // Testing data, label
-    Array3<f64>, Array2<f64>    // Validation data, label
+    Array2<f64>, Array2<f64>,   // Training data, label
+    Array2<f64>, Array2<f64>,   // Testing data, label
+    Array2<f64>, Array2<f64>    // Validation data, label
 ){
     let Mnist {
         trn_img,
@@ -29,29 +29,25 @@ fn create_mnist_dataset(trn_len: u32, tst_len: u32, val_len: u32) -> (
         .validation_set_length(val_len)
         .finalize();
 
-
-    // TODO: change this to 50_000 by 784 so that its 1d data
-    // Can use an Array2 or Array3 here (Array3 for visualization)
-    let train_data = Array3::from_shape_vec((50_000, 28, 28), trn_img)
-        .expect("Error converting images to Array3 struct")
+    let train_data = Array2::from_shape_vec((50_000, 784), trn_img)
+        .expect("Error converting images to Array2 struct")
         .map(|x| *x as f64 / 256.);
 
-    // Convert the returned Mnist struct to Array2 format
-    let trn_lbl: Vec<f64> = trn_lbl.iter().map(|x| *x as f64).collect();
+    let trn_lbl: Vec<usize> = trn_lbl.iter().map(|x| *x as usize).collect();
     let train_labels: Array2<f64> = one_hot_encoding_vec(&trn_lbl, 9);
 
-    let test_data = Array3::from_shape_vec((10_000, 28, 28), tst_img)
-        .expect("Error converting images to Array3 struct")
+    let test_data = Array2::from_shape_vec((10_000, 784), tst_img)
+        .expect("Error converting images to Array2 struct")
         .map(|x| *x as f64 / 256.);
 
-    let tst_lbl: Vec<f64> = tst_lbl.iter().map(|x| *x as f64).collect();
+    let tst_lbl: Vec<usize> = tst_lbl.iter().map(|x| *x as usize).collect();
     let test_labels: Array2<f64> = one_hot_encoding_vec(&tst_lbl, 9);
 
-    let val_data = Array3::from_shape_vec((10_000, 28, 28), val_img)
-        .expect("Error converting images to Array3 struct")
+    let val_data = Array2::from_shape_vec((10_000, 784), val_img)
+        .expect("Error converting images to Array2 struct")
         .map(|x| *x as f64 / 256.);
 
-    let val_lbl: Vec<f64> = val_lbl.iter().map(|x| *x as f64).collect();
+    let val_lbl: Vec<usize> = val_lbl.iter().map(|x| *x as usize).collect();
     let val_labels: Array2<f64> = one_hot_encoding_vec(&val_lbl, 9);
 
     (train_data, train_labels, test_data, test_labels, val_data, val_labels)
